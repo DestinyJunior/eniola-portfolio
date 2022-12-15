c05cb402fb73dddeafe796231e1d8d7476219d4b
Destiny Junior <destinyjuniorsdj@gmail.com>
2022-12-15 01:16:28 +0100

    feat: desktop designs done

---
+++ src/Home.vue
@@ -0,0 +1,269 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="pt-[79px]">
+        <nav class="flex items-center justify-between px-[110px]">
+            <div>
+                <img src="/images/png/logo.png" class="h-[113px] w-[201px]" alt="Eniola">
+            </div>
+            <!-- links -->
+            <ul class="flex space-x-[47px]">
+                <li class="text-[#EB6854] text-2xl font-semibold">Portfolio</li>
+                <li class="text-2xl font-normal">About me</li>
+                <li class="text-2xl font-normal">Contact</li>
+            </ul>
+        </nav>
+        <div class="pb-[120px] py-[65px] mx-auto">
+            <p class="font-light text-[56px] text-center tracking-wide leading-[150%]">
+                Hello! I’m <span class="text-[#EB6854] font-semibold">Eniola Ajiboye</span>. A <br> skilled product
+                designer
+                based
+                <br> in Nigeria
+            </p>
+        </div>
+
+        <!-- selected works -->
+        <section class="space-y-[68px] px-[110px]">
+            <div>
+                <h4 class="text-[40px] font-light"><span class="opacity-50">Selected</span> <span
+                        class="text-[#EB6854] font-semibold">Works</span></h4>
+            </div>
+
+            <div>
+                <div class="flex items-center space-x-[48px] mx-auto">
+                    <img src="/images/png/farmers-dao.png" alt="farmersDao" />
+
+                    <div class="space-y-[16px]">
+                        <div>
+                            <h5 class="text-base font-light text-[#EB6854]">Web design</h5>
+                            <h4 class="text-2xl font-semibold">Farmer’s DAO</h4>
+                        </div>
+
+                        <div class="space-y-[24px]">
+                            <p class="text-[#333333] tracking-tight text-[32px] leading-[150%]">A digital platform that
+                                helps user find
+                                <br> tourists
+                                centre and event
+                                happening <br> in a
+                                particular city
+                            </p>
+
+                            <button class="bg-[#EB6854] text-white rounded-[40px] py-[15px] px-7 tracking-wider">View
+                                project</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--  -->
+
+            <div>
+                <div class="flex items-center justify-end space-x-[48px] mx-auto">
+                    <div class="space-y-[16px]">
+                        <div>
+                            <h5 class="text-base font-light text-[#EB6854]">Mobile app</h5>
+                            <h4 class="text-2xl font-semibold">Service Provider</h4>
+                        </div>
+
+                        <div class="space-y-[24px]">
+                            <p class="text-[#333333] tracking-tight text-[32px] leading-[150%]">A digital platform that
+                                helps user find
+                                <br> tourists
+                                centre and event
+                                happening <br> in a
+                                particular city
+                            </p>
+
+                            <button class="bg-[#EB6854] text-white rounded-[40px] py-[15px] px-7 tracking-wider">View
+                                project</button>
+                        </div>
+                    </div>
+                    <img src="/images/png/service-provider.png" alt="farmersDao" />
+                </div>
+            </div>
+            <!--  -->
+            <div>
+                <div class="flex items-center space-x-[48px] mx-auto">
+                    <img src="/images/png/unoma.png" alt="UNOMA" />
+
+                    <div class="space-y-[16px]">
+                        <div>
+                            <h5 class="text-base font-light text-[#EB6854]">Dashboard</h5>
+                            <h4 class="text-2xl font-semibold">UNOMA</h4>
+                        </div>
+
+                        <div class="space-y-[24px]">
+                            <p class="text-[#333333] tracking-tight text-[32px] leading-[150%]">A digital platform that
+                                helps user find
+                                <br> tourists
+                                centre and event
+                                happening <br> in a
+                                particular city
+                            </p>
+
+                            <button class="bg-[#EB6854] text-white rounded-[40px] py-[15px] px-7 tracking-wider">View
+                                project</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--  -->
+
+            <div>
+                <div class="flex items-center justify-end space-x-[48px] mx-auto">
+                    <div class="space-y-[16px]">
+                        <div>
+                            <h5 class="text-base font-light text-[#EB6854]">Web design</h5>
+                            <h4 class="text-2xl font-semibold">Eventour360</h4>
+                        </div>
+
+                        <div class="space-y-[24px]">
+                            <p class="text-[#333333] tracking-tight text-[32px] leading-[150%]">A digital platform that
+                                helps user find
+                                <br> tourists
+                                centre and event
+                                happening <br> in a
+                                particular city
+                            </p>
+
+                            <button class="bg-[#EB6854] text-white rounded-[40px] py-[15px] px-7 tracking-wider">View
+                                project</button>
+                        </div>
+                    </div>
+                    <img src="/images/png/eventour.png" alt="Eventour360" />
+                </div>
+            </div>
+            <!--  -->
+            <div>
+                <div class="flex items-center space-x-[48px] mx-auto">
+                    <img src="/images/png/grow-with-igho.png" alt="farmersDao" />
+
+                    <div class="space-y-[16px]">
+                        <div>
+                            <h5 class="text-base font-light text-[#EB6854]">Landing page</h5>
+                            <h4 class="text-2xl font-semibold">IGOWITHIGHO</h4>
+                        </div>
+
+                        <div class="space-y-[24px]">
+                            <p class="text-[#333333] tracking-tight text-[32px] leading-[150%]">A digital platform that
+                                helps user find
+                                <br> tourists
+                                centre and event
+                                happening <br> in a
+                                particular city
+                            </p>
+
+                            <button class="bg-[#EB6854] text-white rounded-[40px] py-[15px] px-7 tracking-wider">View
+                                project</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--  -->
+        </section>
+
+        <!-- about -->
+        <section class="pt-[220px]">
+            <div class="flex items-start space-x-[48px] mx-auto pb-[121px] px-[110px]">
+                <img src="/images/png/eniola.png" alt="Eniola" class="-mt-24" />
+
+                <div class="space-y-[16px]">
+                    <div class="space-y-[63px]">
+                        <div class="text-[#333333] text-2xl font-normal">
+                            <p class="leading-[150%]">I am a collaborative designer with
+                                a
+                                creative mind; I am fascinated in creating designs that are both functional and
+                                visually
+                                beautiful. I have a solid understanding of design principles such as colour theory,
+                                layout, white space and typography.
+                            </p>
+                            <br>
+                            <p class="leading-[150%]"> I am skilled in a number of design software packages, including
+                                Adobe Illustrator,
+                                Adobe
+                                Photoshop and Corel Draw.</p>
+                            <br>
+                            <p class="leading-[150%]">I love making new friends and understanding how they solve
+                                problems; which is one of
+                                the
+                                reasons why I invest my time in gatherings ranging from product management to
+                                everything
+                                related to design (research, design thinking, design system, etc.) and talking to
+                                people
+                                in general, as every opportunity to learn something new is always welcome. When I'm
+                                building my solutions, I see it as an opportunity to grow myself as a professional
+                                and
+                                put things into practice.</p>
+                        </div>
+
+                        <button class="bg-[#EB6854] text-white rounded-[40px] py-[15px] px-7 tracking-wider">Download
+                            CV</button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- tools -->
+
+            <div class="flex items-center justify-center space-x-[120px] py-[65px] bg-pink-light">
+                <img src="/images/png/figma.png" alt="Figma">
+                <img src="/images/png/illustrator.png" alt="Ai">
+                <img src="/images/png/photoshop.png" alt="Ps">
+                <img src="/images/png/xd.png" alt="Xd">
+                <img src="/images/png/lightroom.png" alt="Lr">
+            </div>
+        </section>
+
+        <section class="relative">
+            <div class="absolute flex flex-col space-y-[51px] left-[71px] top-[28%]">
+                <a>
+                    <img src="/images/svg/mail.svg" alt="Email">
+                </a>
+                <a>
+                    <img src="/images/svg/twitter.svg" alt="Twitter">
+                </a>
+                <a>
+                    <img src="/images/svg/instagram.svg" alt="Instagram">
+                </a>
+            </div>
+            <div class="pt-[120px] pb-[165px] flex flex-col items-center space-y-[131px]">
+                <div class="space-y-[65px]">
+                    <div class="space-y-[40px]">
+                        <h2 class="text-[64px] font-semibold tracking-tight text-[#333333]">Take A Coffee & Chat With Me
+                        </h2>
+                        <p class="text-center text-[32px] font-light leading-[150%]">See out what happens when great
+                            projects <br>
+                            are
+                            combined with
+                            amazing
+                            design.
+                        </p>
+                    </div>
+
+                    <div class="w-full flex space-x-[78px]">
+                        <div
+                            class="w-[435px] flex items-center justify-center bg-pink-light-100 py-[30px] rounded-[20px] space-x-[32px]">
+                            <img src="/images/png/opened-envelope.png" alt="Email">
+                            <span class="text-2xl font-medium text-[#5E5E5E]">ajiboyeniola@gmail.com</span>
+                        </div>
+                        <div
+                            class="w-[435px] flex items-center justify-center bg-pink-light-100 py-[30px] rounded-[20px] space-x-[32px]">
+                            <img src="/images/png/call.png" alt="Email">
+                            <span class="text-2xl font-medium text-[#5E5E5E]">ajiboyeniola@gmail.com</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="text-center space-y-3 text-2xl font-normal">
+                    <p>© 2022 Ajiboye Eniola</p>
+                    <p>Designed and implemented by
+                        <br> Eniola & Destiny
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
--- windi.config.ts
+++ windi.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'windicss/helpers';
+import formsPlugin from 'windicss/plugin/forms';
 
 export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        'pink-light': 'rgba(235, 104, 84, 0.13)',
+        'pink-light-100': 'rgba(235, 104, 84, 0.08)',
+      },
+    },
+  },
   plugins: [],
 });
